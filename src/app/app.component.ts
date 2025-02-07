64e678aebf6a76f82d10da143e5f3ce1bbe29e97
caballerito22 <alecabnun@alu.edu.gva.es>
2025-02-07 09:54:53 +0100

    luidmili

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-raiz',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'miprimeraApp';
+
+  saludo="Hola alumnos de Angular. Qué tal estáis?"
 }
