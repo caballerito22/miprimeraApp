b2eeaa8de14aed8b6994822ff9b5e0f833c3ac8a
caballerito22 <alecabnun@alu.edu.gva.es>
2025-02-10 14:39:46 +0100

    video 8

---
+++ src/app/empleado/empleado.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-empleado',
+  imports: [],
+  templateUrl: './empleado.component.html',
+  //template:"<p>Aquí iría un empleado</p>",
+  styleUrl: './empleado.component.css'
+  //styles: ["p { background-color: red; }"]
+
+})
+export class EmpleadoComponent implements OnInit{
+  constructor(){}
+
+  nombre="Alejandro";
+
+  apellido="Caballero";
+
+  edad=19;
+
+  empresa="Proyecto Lorena"
+
+  /*getEdad(){return this.edad}*/
+
+
+  ngOnInit(): void {
+    
+  }
+}
